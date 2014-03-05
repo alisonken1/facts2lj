3cf8c5c7b156533eccc93384ca9fca338d62b9b7
Ken Roberts <kroberts@abletronics.com>
2014-03-05 08:06:57 -0800

    Added error exit code to temp file creation

--- bash/facts2lj.sh
+++ bash/facts2lj.sh
@@ -16,16 +16,17 @@ echo "Sourcing ${dirScripts}/facts2functions.sh" >&2
 source ${dirScripts}/facts2functions.sh
 #==================================================
 # Save report to temp file
-#fileTmp="$( mktemp ${dirTemp}/${myName}.XXXXXX )"
-fileTmp="$( mktemp /sys/facts2ljtest.XXXXXX)"
-if [ -f ${fileTmp} ]; then
-    cat - >"${fileTmp}" || {
+
+fileTmp="$( mktemp ${dirTemp}/${myName}.XXXXXX )"
+zz=$?
+if [ ${zz} -ne 0 ]; then
+    dbg ${dbg_FATAL} "Cannot create temporary file!!!! Inodes full?"
+    dbg ${dbg_FATAL} "Exiting"
+    exit ${zz}
+fi
+dbg ${dbg_INFO} "Saving to ${fileTmp}"
+cat - >"${fileTmp}" || {
         dbg ${dbg_FATAL} "Cannot create ${fileTmp}!!!! System full?"
         dbg ${dbg_FATAL} "Exiting"
         exit 1
-    }
-else
-    dbg ${dbg_FATAL} "Cannot create temporary file!!!! Inodes full?"
-    dbg ${dbg_FATAL} "Exiting"
-fi
-dbg ${dbg_INFO} "Saving report as temp file ${fileTmp}"
+}
