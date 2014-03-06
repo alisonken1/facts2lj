cda0faf0b3f9e4471b3099e52f90809c1c44cbb0
Ken Roberts <kroberts@abletronics.com>
2014-03-06 11:06:45 -0800

    Added -v option to return versions of all scripts

--- bash/facts2lj.sh
+++ bash/facts2lj.sh
@@ -8,14 +8,29 @@
 # for output to printer ($0 is link for printer queue), with option
 # to save to REPORTS directory for archiving.
 #
+export dirBase="/facts"
+export dirScripts="${dirBase}/bash"
+#
 export __name__="facts2lj"
 export __version__="0.3"
 export __status__="RUN"
+declare -a  __scripts__
+__scripts__[0]="facts2archive"
+__scripts__[1]="facts2invoice"
+export __scripts__
 #
-# Set these two so we know where the rest of the scripts are
-export dirBase="/facts"
-export dirScripts="${dirBase}/bash"
-echo "${myName} ${myVersion} starting initial run" >&2
+# Test for options
+case ${1} in
+-v|--version)
+    unset __status__
+    echo ${__name__} ${__version__}
+    for i in ${__scripts__[*]}; do
+        ${dirScripts}/${i}.sh
+    done
+    exit
+    ;;
+esac
+echo "${__name__} ${__version__} starting initial run" >&2
 echo "Sourcing ${dirScripts}/facts2functions.sh" >&2
 source ${dirScripts}/facts2functions.sh
 # At this point the rest of the functions and variables should be useable
@@ -23,30 +38,34 @@ source ${dirScripts}/facts2functions.sh
 #==================================================
 # Save report to temp file
 
-fileTmp="$( mktemp ${dirTemp}/${myName}.XXXXXX )"
+export fileTmp="$( mktemp ${dirTemp}/${__name__}.XXXXXX )"
 zz=$?
 if [ ${zz} -ne 0 ]; then
     dbg ${dbg_FATAL} "Cannot create temporary file!!!! Inodes full?"
-    dbg ${dbg_FATAL} "Exiting"
+    dbg ${dbg_FATAL} "Exiting mktemp code ${zz}"
+    rmTmpFiles
     exit ${zz}
 fi
 dbg ${dbg_INFO} "Saving to ${fileTmp}"
 cat - >"${fileTmp}"
 zz=$?
 [ ${zz} -ne 0 ] && {
-        dbg ${dbg_FATAL} "Cannot create ${fileTmp}!!!! System full?"
-        dbg ${dbg_FATAL} "Exiting"
+        dbg ${dbg_FATAL} "Cannot save to ${fileTmp}!!!! Filesystem full?"
+        dbg ${dbg_FATAL} "Exiting mktemp code ${zz}"
+        rmTmpFiles
         exit ${zz}
 }
 
 # =================================================
-# Check for output destination
+# Check for special processing outputs
 case ${lpPrinter} in
 archive)
+    # Archive only
     dbg ${dbg_INFO} "Sending data to archive"
     source ${dirScripts}/facts2archive.sh
     ;;
 *)
+
     dbg ${dbg_ERROR} "Unkonwn destination: ${lpPrinter}"
     ;;
 esac
