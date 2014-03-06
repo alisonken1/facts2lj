4579b1281abe1da0d4495a3a88f16eae789dd0bb
Ken Roberts <kroberts@abletronics.com>
2014-03-06 09:03:54 -0800

    Renamed myName/myVersion to __name__/__version

--- bash/facts2lj.sh
+++ bash/facts2lj.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+#
 # Entry point for facts2lj scripts.
 # Called by F.A.C.T.S. via pipe.
 #
@@ -7,13 +8,17 @@
 # for output to printer ($0 is link for printer queue), with option
 # to save to REPORTS directory for archiving.
 #
-export myName="facts2lj"
-export myVersion="0.3"
+export __name__="facts2lj"
+export __version__="0.3"
+#
+# Set these two so we know where the rest of the scripts are
 export dirBase="/facts"
 export dirScripts="${dirBase}/bash"
 echo "${myName} ${myVersion} starting initial run" >&2
 echo "Sourcing ${dirScripts}/facts2functions.sh" >&2
 source ${dirScripts}/facts2functions.sh
+# At this point the rest of the functions and variables should be useable
+#
 #==================================================
 # Save report to temp file
 
@@ -32,3 +37,17 @@ zz=$?
         dbg ${dbg_FATAL} "Exiting"
         exit ${zz}
 }
+
+# =================================================
+# Check for output destination
+case ${lpPrinter} in
+archive)
+    dbg ${dbg_INFO} "Sending data to archive"
+    source ${dirScripts}/facts2archive.sh
+    ;;
+*)
+    dbg ${dbg_ERROR} "Unkonwn destination: ${lpPrinter}"
+    ;;
+esac
+
+rmTmpFiles
